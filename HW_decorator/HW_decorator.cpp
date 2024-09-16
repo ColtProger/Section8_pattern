3f284aa658ce1bb266b6f0b2bf6568d5b20aeee7
alexandra_f <avfriesen@mail.ru>
2024-09-16 22:51:18 +0300

    Section8, HW1 task2 changed

--- HW_decorator/HW_decorator.cpp
+++ HW_decorator/HW_decorator.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <algorithm>
 
+
 class Text {
 public:
     virtual void render(const std::string& data) const {
@@ -12,13 +13,14 @@ public:
 
 class DecoratedText : public Text {
 public:
-    explicit DecoratedText(Text* text) : text_(text) {}
-    Text* text_;
+    explicit DecoratedText(std::shared_ptr<Text> text) : text_(text) {}
+    //Text* text_;
+    std::shared_ptr<Text> text_;
 };
 
 class ItalicText : public DecoratedText {
 public:
-    explicit ItalicText(Text* text) : DecoratedText(text) {}
+    explicit ItalicText(std::shared_ptr<Text> text) : DecoratedText(text) {}
     void render(const std::string& data)  const {
         std::cout << "<i>";
         text_->render(data);
@@ -28,69 +30,68 @@ public:
 
 class BoldText : public DecoratedText {
 public:
-    explicit BoldText(Text* text) : DecoratedText(text) {}
+    explicit BoldText(std::shared_ptr<Text> text) : DecoratedText(text) {}
     void render(const std::string& data) const {
         std::cout << "<b>";
         text_->render(data);
         std::cout << "</b>";
     }
 };
-
+//
 class Paragraph : public DecoratedText {
 public:
 
-    explicit Paragraph(Text* text) : DecoratedText(text) {}
+    explicit Paragraph(std::shared_ptr<Text> text) : DecoratedText(text) {}
     void render(const std::string& data) const {
         std::cout << "<p>";
         text_->render(data);
         std::cout << "</p>";
     }
-
+//
 };
-
+//
 class Reversed : public DecoratedText {
 public:
 
-    explicit Reversed(Text* text) : DecoratedText(text) {}
+    explicit Reversed(std::shared_ptr<Text> text) : DecoratedText(text) {}
     void render(const std::string& data) const override {
         std::string newdata = data;
         
-        std::cout << "<r>";
+       std::cout << "<r>";
         std::reverse(newdata.begin(), newdata.end());
         text_->render(newdata);
         std::cout << "</r>";
     }
-
 };
-
+//
 class Link : public DecoratedText {
 public:
 
-    explicit Link(Text* text) : DecoratedText(text) {}
+    explicit Link(std::shared_ptr<Text> text) : DecoratedText(text) {}
     void render(std::string ref, const std::string& data){
-        std::string newdata = data;
+       std::string newdata = data;
 
         std::cout << "<a href = "<<ref <<">";
         text_->render(data);
         std::cout << "</a>";
     }
-
 };
 
 int main() {
-    auto text_block = new ItalicText(new BoldText(new Text()));
+   
+    auto text_block = std::make_shared< ItalicText>(std::make_shared<Text>());
     text_block->render("Hello world");
 
     std::cout << "\n\n";
-    auto text_block2 = new Paragraph(new Text());
+   auto text_block2 = std::make_shared <Paragraph>(std::make_shared < Text>());
     text_block2->render("Hello world");
 
     std::cout << "\n\n";
-    auto text_block3 = new Reversed(new Text());
+    auto text_block3 = std::make_shared < Reversed>(std::make_shared < Text>());
     text_block3->render("Hello world");
 
     std::cout << "\n\n";
-    auto text_block4 = new Link(new Text());
+    auto text_block4 =  std::make_shared< Link >(std::make_shared<Text>());
     text_block4->render("netology.ru", "Hello world");
 
 }
